30a8fe4af902aa6b2668e6347fc5d47a96ac4f9b
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-01-26 00:17:09 -0300

    Challenge #5

---
+++ src/2023/challenge-05.js
@@ -0,0 +1,30 @@
+function cyberReindeer(road, time) {
+    const raceData = [road];
+  
+    road = road.replace('S', '.');
+  
+    let reindeerPosition = 2;
+    let pointer = 2;
+  
+    let raceTime = 1;
+  
+    while (raceTime < time) {
+      const symbol = road[pointer];
+  
+      raceData.push(
+        road.slice(0, reindeerPosition - 1) + 'S' +
+        road.slice(reindeerPosition)
+      );
+  
+      if (++raceTime === 5) {
+        road = road.replace(/\|/g, '*');
+      } else if (symbol === '|') {
+        continue;
+      }
+  
+      reindeerPosition++;
+      pointer++;
+    }
+  
+    return raceData;
+}  
